2c1c2aba64eff7f45d753d93573c269848334fdf
Paarth Kotak <kotak.p@husky.neu.edu>
2018-02-18 10:54:46 -0500

    initial commit

---
+++ src/main/java/edu/northeastern/cs5200/springbootjdbc/SpringBootJdbcApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5200.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApplication.class, args);
+	}
+}
